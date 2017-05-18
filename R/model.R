b8c3e4af3e93c4cac997960ce0dddee168a108e6
mproeling <mproeling@users.noreply.github.com>
2017-05-18 16:41:22 +0200

    Update model.R

--- R/model.R
+++ R/model.R
@@ -586,7 +586,7 @@ setRefClass("model",
                 
                 print(length(is.na(quals)))
                 print(length(naquals))
-                print("test2")
+                print("test3")
                 # ends here 
                 
                 quals[is.na(quals)] <- naquals          
